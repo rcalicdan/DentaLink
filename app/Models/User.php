da8a8bb37bdcaca2b5e1a1b6fd1b23c585c0208e
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-08-13 22:53:09 +0800

    Add Auditable trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Enums\UserRoles;
+use App\Libraries\Audit\Auditable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Auditable;
 
     protected $fillable = [
         'first_name',
